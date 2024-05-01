5af8bc280ecbe58fc74e612413f32882b5653b5c
sarmad52422 <sarmad52422@gmail.com>
2024-05-01 23:31:52 +0500

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 import {Environment} from '@delon/theme';
 
 export const environment = {
-    production: false,
+    production: true,
     useHash: true,
     api: {
-        baseUrl: 'https://stock-api-tagname.onrender.com',
+        baseUrl: 'https://stocker-api-tagname.onrender.com',
         refreshTokenEnabled: true,
         refreshTokenType: 'auth-refresh'
     },
